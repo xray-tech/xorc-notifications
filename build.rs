8f187c022964e1dea6b62916ca4b80ba24f06b52
Julius de Bruijn <julius.debruijn@360dialog.com>
2016-06-08 15:48:46 +0200

    Build protobuf classes automatically

--- build.rs
+++ build.rs
@@ -3,9 +3,9 @@ use std::process::Command;
 fn main() {
     Command::new("protoc").args(&["--rust_out", "src/events", "--proto_path",
                                 "../../../third_party/events/schema/",
-                                "../../../third_party/events/schema/notification/push_notification.proto"
-                                "../../../third_party/events/schema/notification/apns_result.proto"
-                                "../../../third_party/events/schema/notification/apns_headers.proto"
+                                "../../../third_party/events/schema/notification/push_notification.proto",
+                                "../../../third_party/events/schema/notification/apns_result.proto",
+                                "../../../third_party/events/schema/notification/apns_headers.proto",
     ]).status().unwrap();
 }
 
