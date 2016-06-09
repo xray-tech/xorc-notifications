bac62d6283dcef13930e58072adaea5d63aa2db7
Julius de Bruijn <julius.debruijn@360dialog.com>
2016-06-09 16:18:06 +0200

    Add different APN types

--- src/notifier.rs
+++ src/notifier.rs
@@ -14,20 +14,69 @@ impl Apns2Notifier {
     }
 
     pub fn send(&self, event: &PushNotification) -> AsyncResponse {
-        let alert   = APSAlert::Plain(event.get_message().to_string());
-        let payload = Payload::new(alert, 1, "default");
-        let token   = DeviceToken::new(event.get_device_token());
-        let headers = event.get_apns_headers();
+        let token             = DeviceToken::new(event.get_device_token());
+        let notification_data = event.get_notification();
+        let headers           = notification_data.get_apns_headers();
 
         let options = NotificationOptions {
-            apns_priority: if headers.has_apns_priority() { Some(headers.get_apns_priority()) } else { None },
-            apns_id: if event.has_correlation_id() { Some(event.get_correlation_id()) } else { None },
+            apns_priority:   if headers.has_apns_priority()   { Some(headers.get_apns_priority())   } else { None },
+            apns_id:         if event.has_correlation_id()    { Some(event.get_correlation_id())    } else { None },
             apns_expiration: if headers.has_apns_expiration() { Some(headers.get_apns_expiration()) } else { None },
-            apns_topic: if headers.has_apns_topic() { Some(headers.get_apns_topic()) } else { None }, ..Default::default()
+            apns_topic:      if headers.has_apns_topic()      { Some(headers.get_apns_topic())      } else { None }, ..Default::default()
         };
 
-        let notification = Notification::new(payload, token, options);
+        let badge = if notification_data.has_badge() {
+            notification_data.get_badge()
+        } else { 1u32 };
 
-        self.apns2_provider.push(notification)
+        let sound = if notification_data.has_sound() {
+            notification_data.get_sound()
+        } else { "default" };
+
+        let category = if notification_data.has_category() {
+            Some(notification_data.get_category().to_string())
+        } else { None };
+
+        let payload: Payload = if notification_data.has_localized() {
+            let alert_data = notification_data.get_localized();
+
+            let title_loc_key = if alert_data.has_title_loc_key() {
+                Some(alert_data.get_title_loc_key().to_string())
+            } else { None };
+
+            let title_loc_args = if alert_data.get_title_loc_args().len() > 0 {
+                Some(alert_data.get_title_loc_args().iter().map(|a| a.to_string()).collect())
+            } else { None };
+
+            let action_loc_key = if alert_data.has_action_loc_key() {
+                Some(alert_data.get_action_loc_key().to_string())
+            } else { None };
+
+            let launch_image = if alert_data.has_launch_image() {
+                Some(alert_data.get_launch_image().to_string())
+            } else { None };
+
+            let alert = APSAlert::Localized(
+                APSLocalizedAlert {
+                    title: alert_data.get_title().to_string(),
+                    body: alert_data.get_body().to_string(),
+                    title_loc_key: title_loc_key,
+                    title_loc_args: title_loc_args,
+                    action_loc_key: action_loc_key,
+                    loc_key: alert_data.get_loc_key().to_string(),
+                    loc_args: alert_data.get_loc_args().iter().map(|a| a.to_string()).collect(),
+                    launch_image: launch_image,
+                });
+
+            Payload::new(alert, badge, sound, category)
+        } else if notification_data.has_silent() {
+            Payload::new_silent_notification()
+        } else {
+            let alert = APSAlert::Plain(notification_data.get_plain().to_string());
+
+            Payload::new(alert, badge, sound, category)
+        };
+
+        self.apns2_provider.push(Notification::new(payload, token, options))
     }
 }
