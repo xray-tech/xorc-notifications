e26aeb707991e9eac96774fdccb6684ba6c8a407
Julius de Bruijn <julius.debruijn@360dialog.com>
2016-09-22 18:44:35 +0200

    Changes necessary for moving the project from dmp

--- build.rs
+++ build.rs
@@ -2,11 +2,11 @@ use std::process::Command;
 
 fn main() {
     Command::new("protoc").args(&["--rust_out", "src/events", "--proto_path",
-                                "../../../third_party/events/schema/",
-                                "../../../third_party/events/schema/common/header.proto",
-                                "../../../third_party/events/schema/common/map_field_entry.proto",
-                                "../../../third_party/events/schema/notification/push_notification.proto",
-                                "../../../third_party/events/schema/notification/google_notification.proto",
-                                "../../../third_party/events/schema/notification/apple_notification.proto",
+                                "./third_party/events/schema/",
+                                "./third_party/events/schema/common/header.proto",
+                                "./third_party/events/schema/common/map_field_entry.proto",
+                                "./third_party/events/schema/notification/push_notification.proto",
+                                "./third_party/events/schema/notification/google_notification.proto",
+                                "./third_party/events/schema/notification/apple_notification.proto",
     ]).status().unwrap();
 }
