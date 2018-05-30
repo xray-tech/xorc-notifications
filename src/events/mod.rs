4af8de764a309209edbb84272791088c4d71c817
Julius de Bruijn <julius@nauk.io>
2018-05-30 15:08:44 +0200

    Include FCM

--- src/events/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod push_notification;
-pub mod apple_notification;
-pub mod google_notification;
-pub mod webpush_notification;
-pub mod notification_result;
-pub mod map_field_entry;
-pub mod header;
